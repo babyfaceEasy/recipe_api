ab1809879bceef0da0e9e035c4e859c1b460d894
michael.olakunle <michael.olakunle@konga.com>
2019-02-10 05:37:45 +0100

    setting up of gorilla mux router, as well as testing it.

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+// HomeHandler this responds to the / endpoint
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorilla\n"))
+}
+
 func main() {
-	fmt.Println("Welcome to the api")
+	r := mux.NewRouter()
+
+	// add endpoints here
+	r.HandleFunc("/", HomeHandler)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
